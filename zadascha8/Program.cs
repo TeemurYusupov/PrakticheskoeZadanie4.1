ce04a5d2f92ea569ce33b54b5c0445be032c1430
timur <yusupov-rr22@mail.ru>
2022-09-23 23:59:53 +0700

    чётные до n

---
+++ zadascha8/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N
+
+Console.Clear();
+Console.Write("Введите число:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int index = 1;
+
+while (index <= n)
+{
+    if (index % 2 == 0)
+    {
+        Console.Write($" {index}");
+    }
+    index++;
+}
+
+
+
