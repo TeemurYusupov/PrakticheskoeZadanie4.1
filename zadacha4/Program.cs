f50e35676fcff3516aff69e5c66cbe3cb40f9c4e
timur <yusupov-rr22@mail.ru>
2022-09-23 21:25:13 +0700

    max to 3

---
+++ zadacha4/Program.cs
@@ -0,0 +1,47 @@
+﻿//  Напишите программу, которая принимает на вход три числа 
+//и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.Write("Введите число1:  ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число2:  ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число3:  ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+
+
+if ((num1 > num2) || (num1 > num3))
+
+{
+   Console.WriteLine("max =" + num1);
+
+   if ((num2 > num1) || (num2 > num3))
+    {
+        Console.WriteLine("max =" + num2);
+    }
+}
+else
+{
+Console.WriteLine("max =" + num3);
+}
+
+
+//if ((num2 > num1) || (num2 > num3))
+//{
+   // Console.WriteLine("max =" + num2);
+//}
+
+
+//if ((num3 > num1) || (num3 > num2))
+//{
+    //Console.WriteLine("max =" + num3);
+//}
+
+
+
+
+
+
