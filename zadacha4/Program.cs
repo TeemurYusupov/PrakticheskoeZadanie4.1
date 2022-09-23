d0f5647720724af6c554b081db3f2d94aac1d6eb
timur <yusupov-rr22@mail.ru>
2022-09-23 21:58:28 +0700

    redacted

--- zadacha4/Program.cs
+++ zadacha4/Program.cs
@@ -13,35 +13,15 @@ int num3 = Convert.ToInt32(Console.ReadLine());
 
 
 
-if ((num1 > num2) || (num1 > num3))
-
+if (num1 > num2 & num1 > num3)
 {
-   Console.WriteLine("max =" + num1);
-
-   if ((num2 > num1) || (num2 > num3))
-    {
-        Console.WriteLine("max =" + num2);
-    }
+    Console.WriteLine($"max = {num1}");
 }
-else
+if (num2 > num1 & num2 > num3)
 {
-Console.WriteLine("max =" + num3);
+    Console.WriteLine($"max = {num2}");
 }
-
-
-//if ((num2 > num1) || (num2 > num3))
-//{
-   // Console.WriteLine("max =" + num2);
-//}
-
-
-//if ((num3 > num1) || (num3 > num2))
-//{
-    //Console.WriteLine("max =" + num3);
-//}
-
-
-
-
-
-
+if (num3 > num1 & num3 > num2)
+{
+    Console.WriteLine($"max = {num3}");
+}
